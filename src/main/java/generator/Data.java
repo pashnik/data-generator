d4d57b1742e86188dbe9941bad2ab84eeada9ccb
Pashnik <pashanikon15@gmail.com>
2019-04-05 13:23:39 +0300

    working on generator

--- src/main/java/generator/Data.java
+++ src/main/java/generator/Data.java
@@ -5,36 +5,46 @@ import java.util.List;
 
 public class Data {
 
-    public static final List<String> countries = Arrays.asList("Italy",
+    public static final int MIN_ENGINE_POWER = 150;
+    public static final int MAX_ENGINE_POWER = 500;
+
+    public static final int MIN_WORKING_VOLUME = 1;
+    public static final int MAX_WORKING_VOLUME = 4;
+
+    public static final int MIN_ECOLOGICAL_CLASS = 2;
+    public static final int MAX_ECOLOGICAL_CLASS = 5;
+
+
+    public static final List<String> COUNTRIES = Arrays.asList("Italy",
             "Russia", "Germany", "Japan", "USA", "France");
 
-    public static final List<String> manufacturers = Arrays.asList("Toyota",
+    public static final List<String> MANUFACTURERS = Arrays.asList("Toyota",
             "BMW", "Mercedes", "Porshe", "Kia", "Lamborgini", "Skoda",
             "Volkswagen", "Nissan", "Pegeuot", "Land Rover");
 
-    public static final List<String> prefixCarName = Arrays.asList("Ur",
+    public static final List<String> PREFIX_CAR_NAME = Arrays.asList("Ur",
             "Li", "A", "Gl", "Nos", "Liof", "Urus", "Pr", "Gf", "Nt", "Pe",
             "Nc", "Se", "Ee", "Yg", "Ko", "Mze", "Lix");
 
-    public static final List<String> postfixCarName = Arrays.asList("U1",
+    public static final List<String> POSTFIX_CAR_NAME = Arrays.asList("U1",
             "R4", "O2", "U0", "C2", "I9", "B2", "C4", "U6", "Ce8",
             "L0", "L8", "H1", "S5");
 
-    public static final List<String> transmissionType = Arrays.asList("firstType",
-            "secondType", "thirdType");
+    public static final List<String> TRANSMISSION_TYPE = Arrays.asList("Mechanical",
+            "Automatic");
 
-    public static final List<String> bodyType = Arrays.asList("firstType",
-            "seondType", "thirdType", "fourthType");
+    public static final List<String> BODY_TYPE = Arrays.asList("Sedan",
+            "Hatchback", "Coupe", "Limousine", "Cabriolet", "Minivan");
 
-    public static final List<String> prefixOptional = Arrays.asList("QNX",
+    public static final List<String> PREFIX_OPTIONAL = Arrays.asList("QNX",
             "Aqu-Sys", "GoodNf", "ElseRouteX", "NouQx", "NGINX-S", "PolQ",
             "Ovi-Oks", "Lopte-L", "Kgq-rf", "Kgv-Ls", "NourentGq", "ListYou",
             "OkoRt-t", "NsbgIO", "LkRw", "ZOPQ-a");
 
-    public static final List<String> postfixOptional = Arrays.asList("O8",
+    public static final List<String> POSTFIX_OPTIONAL = Arrays.asList("O8",
             "9I", "Ic", "ce3", "asd2", "iY6", "90", "op1", "La2", "p4", "yt4");
 
-    public static final List<String> engineType = Arrays.asList("firstType",
-            "secondType", "ThirdType");
+    public static final List<String> ENGINE_TYPE = Arrays.asList("Petrol",
+            "Diesel");
 
 }
--- src/main/java/generator/DataGenerator.java
+++ src/main/java/generator/DataGenerator.java
@@ -1,10 +1,7 @@
 package generator;
 
 import dao.*;
-import models.Body;
-import models.Car;
-import models.Manufacturer;
-import models.Transmission;
+import models.*;
 
 public class DataGenerator {
 
@@ -36,18 +33,27 @@ public class DataGenerator {
         this.optionalNumber = optionalNumber;
     }
 
+    /*
+    (int) (Math.random() * n) generates a number from 0 to n-1.
+     */
+
     public void generate() {
         generateManufacturer();
         generateCar();
+        generateTransmission();
+        generateBody();
+        generateComplectation();
+
+        // generate bindings to optional and engine!
     }
 
     private void generateManufacturer() {
         for (int i = 0; i < manufacturerNumber; i++) {
             Manufacturer manufacturer = new Manufacturer();
-            manufacturer.setManufacturerName(Data.manufacturers.get((int) (Math.random() *
-                    Data.manufacturers.size())));
-            manufacturer.setCountryName(Data.countries.get((int) (Math.random() *
-                    Data.countries.size())));
+            manufacturer.setManufacturerName(Data.MANUFACTURERS.get((int) (Math.random() *
+                    Data.MANUFACTURERS.size())));
+            manufacturer.setCountryName(Data.COUNTRIES.get((int) (Math.random() *
+                    Data.COUNTRIES.size())));
             manufacturerDao.save(manufacturer);
         }
     }
@@ -55,12 +61,12 @@ public class DataGenerator {
     private void generateCar() {
         for (int i = 0; i < carNumber; i++) {
             Car car = new Car();
-            String carName = Data.prefixCarName.get((int) (Math.random() * Data.prefixCarName.size())) +
-                    Data.postfixCarName.get((int) (Math.random() *
-                            Data.postfixCarName.size()));
+            String carName = Data.PREFIX_CAR_NAME.get((int) (Math.random() * Data.PREFIX_CAR_NAME.size())) +
+                    Data.POSTFIX_CAR_NAME.get((int) (Math.random() *
+                            Data.POSTFIX_CAR_NAME.size()));
 
             car.setName(carName);
-            car.setManufacturer(manufacturerDao.findById((int) (Math.random() * manufacturerNumber) + 1)); //check
+            car.setManufacturer(manufacturerDao.findById((int) (Math.random() * manufacturerNumber) + 1));
             carDao.save(car);
         }
     }
@@ -68,29 +74,71 @@ public class DataGenerator {
     private void generateTransmission() {
         for (int i = 0; i < transmissionNumber; i++) {
             Transmission transmission = new Transmission();
-            transmission.setTransmissionType(Data.transmissionType.get((int) (Math.random() *
-                    Data.transmissionType.size())));
+            transmission.setTransmissionType(Data.TRANSMISSION_TYPE.get((int) (Math.random() *
+                    Data.TRANSMISSION_TYPE.size())));
+            transmissionDao.save(transmission);
         }
     }
 
     private void generateBody() {
         for (int i = 0; i < bodyNumber; i++) {
             Body body = new Body();
-            body.setBodyType(Data.bodyType.get((int) (Math.random() *
-                    Data.bodyType.size())));
+            body.setBodyType(Data.BODY_TYPE.get((int) (Math.random() *
+                    Data.BODY_TYPE.size())));
+            bodyDao.save(body);
         }
     }
 
     private void generateOptional() {
-        // TODO
+        for (int i = 0; i < optionalNumber; i++) {
+            Optional optional = new Optional();
+            optional.setMediaCenter(getOptionalSystem());
+            optional.setAirBags(getOptionalSystem());
+            optional.setEmergencySystem(getOptionalSystem());
+            optional.setHeadlights(getOptionalSystem());
+            optional.setSecurityAlarm(getOptionalSystem());
+
+            optional.addComplectation(complectationDao.findById((int) (Math.random() *
+                    complectationNumner) + 1));
+            optionalDao.save(optional);
+        }
+    }
+
+    private String getOptionalSystem() {
+        return Data.PREFIX_OPTIONAL.get((int) (Math.random() *
+                Data.PREFIX_OPTIONAL.size())) +
+                Data.POSTFIX_OPTIONAL.get((int) (Math.random()) *
+                        Data.POSTFIX_OPTIONAL.size());
     }
 
     private void generateEngine() {
-        // TODO
+        for (int i = 0; i < engineNumber; i++) {
+            Engine engine = new Engine();
+            engine.setPower((int) (Math.random() * (Data.MAX_ENGINE_POWER - Data.MIN_ENGINE_POWER)) +
+                    Data.MIN_ENGINE_POWER + 1);
+            engine.setWorkingVolume((int) (Math.random() * (Data.MAX_WORKING_VOLUME - Data.MIN_WORKING_VOLUME)) +
+                    Data.MIN_WORKING_VOLUME + 1);
+            engine.setEngineType(Data.ENGINE_TYPE.get((int) (Math.random() * (Data.ENGINE_TYPE.size()))));
+            engine.setEcologicalClass((int) (Math.random() * (Data.MAX_ECOLOGICAL_CLASS -
+                    Data.MIN_ECOLOGICAL_CLASS)) + Data.MIN_ECOLOGICAL_CLASS + 1);
+
+            engine.addComplectation(complectationDao.findById((int) (Math.random() *
+                    complectationNumner) + 1));
+            engineDao.save(engine);
+        }
     }
 
     private void generateComplectation() {
-
+        for (int i = 0; i < complectationNumner; i++) {
+            Complectation complectation = new Complectation();
+            complectation.setTransmission(transmissionDao.findById((int) (Math.random() *
+                    transmissionNumber) + 1));
+            complectation.setBody(bodyDao.findById((int) (Math.random() *
+                    bodyNumber) + 1));
+            complectation.setCar(carDao.findById((int) (Math.random() *
+                    carNumber) + 1));
+            complectationDao.save(complectation);
+        }
     }
 
 
